76879417fb7e5679a956977b8c72fc16a37ce76c
acerdeepin <acerdeepin@qq.com>
2019-11-26 15:14:59 +0800

    flustars

--- lib/modules/lib/flustars.dart
+++ lib/modules/lib/flustars.dart
@@ -6,6 +6,23 @@ import 'package:flutter/material.dart';
 //https://github.com/Sky24n/flustars
 
 
+/*Flutter工具类库 flustars
+
+1、SpUtil : 单例"同步"SharedPreferences工具类。支持get传入默认值，支持存储对象，支持存储对象数组。
+2、ScreenUtil : 屏幕适配，获取屏幕宽、高、密度，AppBar高，状态栏高度，屏幕方向.
+3、WidgetUtil : 监听Widget渲染状态，获取Widget宽高，在屏幕上的坐标，获取网络/本地图片尺寸.
+4、DioUtil : 单例Dio网络工具类(已迁移至此处DioUtil)。
+Dart常用工具类库 common_utils
+
+1、TimelineUtil : 时间轴.(新)
+2、TimerUtil : 倒计时，定时任务.(新)
+3、MoneyUtil : 精确转换，元转分，分转元，支持格式输出.(新)
+4、LogUtil : 简单封装打印日志.(新)
+5、DateUtil : 日期转换格式化输出.
+6、RegexUtil : 正则验证手机号，身份证，邮箱等等.
+7、NumUtil : 保留x位小数, 精确加、减、乘、除, 防止精度丢失.
+8、ObjectUtil : 判断对象是否为空(String List Map),判断两个List是否相等.*/
+
 
 /*
 |-- city_select_page.dart 城市列表(索引&悬停)示例
