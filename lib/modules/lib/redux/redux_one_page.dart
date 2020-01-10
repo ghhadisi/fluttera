5bb61395e8b270f151faeb6c523ebd21fdcd7505
acermac <acermac@qq.com>
2020-01-10 17:57:49 +0800

    redux 注释

--- lib/modules/lib/redux/redux_one_page.dart
+++ lib/modules/lib/redux/redux_one_page.dart
@@ -9,7 +9,23 @@ import 'redux_two_page.dart';
 /*
 https://blog.csdn.net/xcf111/article/details/90752577
 https://pub.dev/packages/flutter_redux#-readme-tab-
-*
+
+
+如上图，Redux 的主要由三部分组成:Store 、Action 、 Reducer 。
+
+  Action 用于定义一个数据变化的请求行为。
+  Reducer 用于根据 Action 产生新状态，一般是一个方法。
+  Store 用于存储和管理 state。
+
+
+所以一般流程为:
+1、Widget 绑定了 Store 中的 state 数据。
+2、Widget 通过 Action 发布一个动作。
+3、Reducer 根据 Action 更新 state。
+4、更新 Store 中 state 绑定的 Widget。
+
+根据这个流程，首先我们要创建一个 Store 。如下图，创建 Store 需要 际上是一个带有 state 和 action 的方法，并返回新的 State 。
+
 * */
 class ReduxOnePage extends StatelessWidget{
   @override
